f145f2a751a4d1eb63452f3de7a198da2b02e51e
admin <cdastagirireddy@hotmail.com>
2018-03-28 09:21:42 +0530

    INformation java

--- app/src/main/java/com/kuncham/kidseducationalapp/Information.java
+++ app/src/main/java/com/kuncham/kidseducationalapp/Information.java
@@ -1,7 +1,10 @@
 package com.kuncham.kidseducationalapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Information extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class Information extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+
+
+
+        ImageView home_image = (ImageView)findViewById(R.id.gohome_image);
+        home_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeIntent = new Intent(Information.this,HomePage.class);
+                startActivity(homeIntent);
+            }
+        });
     }
 }
