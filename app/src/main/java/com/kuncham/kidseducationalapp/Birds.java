a8a6f16fecb55e8751ee58c62d7e436c7d3fad79
admin <cdastagirireddy@hotmail.com>
2018-03-28 09:19:37 +0530

    Birds java

--- app/src/main/java/com/kuncham/kidseducationalapp/Birds.java
+++ app/src/main/java/com/kuncham/kidseducationalapp/Birds.java
@@ -1,13 +1,51 @@
 package com.kuncham.kidseducationalapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
-public class Birds extends AppCompatActivity {
+public class Birds extends AppCompatActivity implements View.OnClickListener{
+
+    ViewFlipper viewFlipper;
+    ImageView prev,next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_birds);
+
+       // getSupportActionBar().hide();
+
+        viewFlipper = (ViewFlipper)findViewById(R.id.viewFlipper);
+        prev = (ImageView)findViewById(R.id.prev_button);
+        next = (ImageView)findViewById(R.id.next_button);
+
+        prev.setOnClickListener(this);
+        next.setOnClickListener(this);
+
+        ImageView home_image = (ImageView)findViewById(R.id.gohome_image);
+        home_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeIntent = new Intent(Birds.this,HomePage.class);
+                startActivity(homeIntent);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view == next)
+        {
+            viewFlipper.showNext();
+        }
+        else if (view == prev)
+        {
+            viewFlipper.showPrevious();
+        }
     }
 }
